bb691d22b348790b2a12ed853c124b82f9018b6e
seohyon-cho <flanique@naver.com>
2024-01-26 18:20:34 +0900

    tailwind로 기본 스타일 지정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 const Header = () => {
 	return (
-		<header>
-			<h1>Netflix</h1>
-			<ul>
-				<li className='headerLink'>menu</li>
-				<li className='headerLink'>menu</li>
-				<li className='headerLink'>menu</li>
-			</ul>
+		<header className='w-[100%]'>
+			<div className='w-[100%] flex justify-between items-center space-x-2 p-5'>
+				<h1>Netflix</h1>
+
+				<ul className='flex gap-[32px]'>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+				</ul>
+			</div>
 		</header>
 	);
 };
